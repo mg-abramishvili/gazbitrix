3a515b4e530c4450872c04617b6ad619c7a04d54
Mikhail Abramishvili <mg@abramishvili.net>
2022-12-13 01:07:11 +0400

    .

--- .left_press.menu.php
+++ .left_press.menu.php
@@ -23,7 +23,7 @@ $aMenuLinks = Array(
 	),
 	Array(
 		"Видео", 
-		"/", 
+		"/press/video/", 
 		Array(), 
 		Array(), 
 		"" 
---
+++ press/video/index.php
@@ -0,0 +1,78 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Title");
+?>
+
+<h5 class="title-head mt-0">Видео</h5>
+    <div class="videos">
+        <div class="row">
+            <div class="col-12 col-lg-6">
+                <div class="video-list-item">
+                    <a href="#">
+                        <div class="video-list-item-image" style="background-image: url(/bitrix/templates/gtu/img/news/1.jpg)">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-circle-fill" viewBox="0 0 16 16">
+                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z"/>
+                            </svg>
+                        </div>
+                        <span>24.08.2022</span>
+                        <h2 class="title-head title-head-blue">ООО «Газпром трансгаз УФА» – в числе лучших обучающих организаций по охране труда</h2>
+                    </a>
+                </div>
+            </div>
+            <div class="col-12 col-lg-6">
+                <div class="video-list-item">
+                    <a href="#">
+                        <div class="video-list-item-image" style="background-image: url(/bitrix/templates/gtu/img/news/1.jpg)">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-circle-fill" viewBox="0 0 16 16">
+                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z"/>
+                            </svg>
+                        </div>
+                        <span>24.08.2022</span>
+                        <h2 class="title-head title-head-blue">ООО «Газпром трансгаз УФА» – в числе лучших обучающих организаций по охране труда</h2>
+                    </a>
+                </div>
+            </div>
+            <div class="col-12 col-lg-6">
+                <div class="video-list-item">
+                    <a href="#">
+                        <div class="video-list-item-image" style="background-image: url(/bitrix/templates/gtu/img/news/1.jpg)">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-circle-fill" viewBox="0 0 16 16">
+                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z"/>
+                            </svg>
+                        </div>
+                        <span>24.08.2022</span>
+                        <h2 class="title-head title-head-blue">ООО «Газпром трансгаз УФА» – в числе лучших обучающих организаций по охране труда</h2>
+                    </a>
+                </div>
+            </div>
+            <div class="col-12 col-lg-6">
+                <div class="video-list-item">
+                    <a href="#">
+                        <div class="video-list-item-image" style="background-image: url(/bitrix/templates/gtu/img/news/1.jpg)">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-circle-fill" viewBox="0 0 16 16">
+                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z"/>
+                            </svg>
+                        </div>
+                        <span>24.08.2022</span>
+                        <h2 class="title-head title-head-blue">ООО «Газпром трансгаз УФА» – в числе лучших обучающих организаций по охране труда</h2>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="pager">
+            <a href="#">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-short" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"/>
+                </svg>
+            </a>
+            <a href="#">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/>
+                </svg>
+            </a>
+        </div>
+    </div>
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+
